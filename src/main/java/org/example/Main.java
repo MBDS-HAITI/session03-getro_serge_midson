5394f144b836449bd7bb32255e1faa0bf999e75c
SergeB101 <sergebeauboeuf5@gmail.com>
2025-11-11 22:18:31 -0500

    BB ----------> LL

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     static void main() {
-        System.out.println("Sak pase Direkte Beauboeuf ....");
+        System.out.println("Sak pase Direkte LAJEANTY ....");
     }
 }
