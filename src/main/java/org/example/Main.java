c84dcaa1495e1ace7f6297b9c73d59a6be16e45d
SergeB101 <sergebeauboeuf5@gmail.com>
2025-11-11 22:19:55 -0500

    ADD --------> BU

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     static void main() {
         System.out.println("Sak pase Direkte LAJEANTY ....");
+        System.out.println("Sak pase Direkte BUISSERETH ....");
     }
 }
